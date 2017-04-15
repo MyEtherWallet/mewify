6ea1477cdbc495e7e6dee27f2338594e032b031b
danny <skubakdj@gmail.com>
2017-04-15 14:44:44 -0400

    fix typo

--- src/scripts-native/98-autoReload.js
+++ src/scripts-native/98-autoReload.js
@@ -5,4 +5,4 @@ gulp.task('reload', function() {
 gulp.watch('./app/js/mewify-master.min.js', ['reload']);
 gulp.watch('./app/js/mewify-native.min.js', ['reload']);
 gulp.watch('./app/css/mewify-master.min.css', ['reload']);
-gulp.watch('./app/index.htmk', ['reload']);
+gulp.watch('./app/index.html', ['reload']);
