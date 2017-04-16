92651e8fade201ba706cd29cae9fb9a133e8b7d0
danny <skubakdj@gmail.com>
2017-04-16 02:25:14 -0400

    add test folder, mocha config, first unit test

---
+++ test/libs/etherUnits.spec.js
@@ -0,0 +1,80 @@
+/* eslint-env mocha */
+
+var path = require('path'),
+    chai = require('chai'),
+    BigNumber = require('bignumber.js'),
+    assert = chai.assert,
+    srcRoot = path.resolve(__dirname + '/../../src'),
+    etherUnits = require(srcRoot + '/scripts/libs/etherUnits');
+
+var validEtherUnits = {
+  'wei'       : '1',
+  'kwei'      : '1000',
+  'ada'       : '1000',
+  'femtoether': '1000',
+  'mwei'      : '1000000',
+  'babbage'   : '1000000',
+  'picoether' : '1000000',
+  'gwei'      : '1000000000',
+  'shannon'   : '1000000000',
+  'nanoether' : '1000000000',
+  'nano'      : '1000000000',
+  'szabo'     : '1000000000000',
+  'microether': '1000000000000',
+  'micro'     : '1000000000000',
+  'finney'    : '1000000000000000',
+  'milliether': '1000000000000000',
+  'milli'     : '1000000000000000',
+  'ether'     : '1000000000000000000',
+  'kether'    : '1000000000000000000000',
+  'grand'     : '1000000000000000000000',
+  'einstein'  : '1000000000000000000000',
+  'mether'    : '1000000000000000000000000',
+  'gether'    : '1000000000000000000000000000',
+  'tether'    : '1000000000000000000000000000000'
+};
+
+var validUnitNames = Object.keys(validEtherUnits);
+
+describe('etherUnits.unitMap', function() {
+  it('should map units correctly', function() {
+    validUnitNames.forEach(function(name) {
+      assert.equal(etherUnits.unitMap[name], validEtherUnits[name]);
+    });
+  });
+});
+
+describe('etherUnits.getValueOfUnit', function() {
+  before(function() {
+    global.BigNumber = BigNumber;
+    global.globalFuncs = {};
+    global.globalFuncs.errorMsgs = [];
+    global.globalFuncs.errorMsgs[4] = '';
+  });
+
+  after(function() {
+    delete global.BigNumber;
+    delete global.globalFuncs;
+  });
+
+  it('should throw an error when bad unit is passed', function() {
+    try {
+      etherUnits.getValueOfUnit('invalid');
+    } catch (e) {
+      return assert.isTrue(e instanceof Error);
+    }
+    assert.isTrue(false) //should return before this
+  });
+
+  it('should default to ether if unit is undefined', function() {
+    assert.equal(etherUnits.getValueOfUnit(), validEtherUnits['ether']);
+  });
+
+  it('should return a BigNumber for all valid units', function() {
+    validUnitNames.forEach(function(name) {
+      assert.isTrue(etherUnits.getValueOfUnit(name) instanceof BigNumber);
+    });
+  });
+
+
+});
