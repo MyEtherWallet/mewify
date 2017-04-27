63ddfe1084571852714bf310cb20ccffdd09f784
danny <skubakdj@gmail.com>
2017-04-27 00:21:10 -0400

    init functional tests

---
+++ test/functional/rpc.watchers.spec.js
@@ -0,0 +1,24 @@
+/* eslint-env mocha */
+
+
+describe('eth_newFilter', function() {
+
+});
+
+describe('eth_newBlockFilter', function() {
+
+});
+
+describe('eth_getFilterChanges', function() {
+
+});
+
+describe('eth_uninstallFilter', function() {
+
+});
+
+describe('eth_newPendingTransactionFilter', function() {
+
+});
+
+//several more also fall under this category 
