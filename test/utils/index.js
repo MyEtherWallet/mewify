ee97af378c0ccd76ea6dfd1946ac885b3c97330e
danny <skubakdj@gmail.com>
2017-04-27 00:57:30 -0400

    add util to validate hex

--- test/utils/index.js
+++ test/utils/index.js
@@ -19,4 +19,17 @@ utils.generateRequestOptions = (o) => {
   return options;
 }
 
+utils.isValidHexResponse = (str) => {
+  if (str.slice(0, 2) !== '0x') return false;
+  str = str.substring(2);
+
+  //inspired by 'felgall' from https://www.sitepoint.com/community/t/how-to-check-if-string-is-hexadecimal/162739/6
+  try {
+    let hex = parseInt(str, 16);
+    return (hex.toString(16) === str.toLowerCase() || hex.toString(16) === '0');
+  } catch(err) {
+    return false;
+  }
+}
+
 module.exports = utils;
