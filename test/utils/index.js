09068c1862e4bcb5f017ce4097e3cddbb6c5da00
danny <skubakdj@gmail.com>
2017-04-27 00:21:52 -0400

    init test utils

---
+++ test/utils/index.js
@@ -0,0 +1,22 @@
+let utils = function(){};
+
+utils.generateRequestOptions = (o) => {
+  //base options to be used on every request
+  let standardOptions = {
+    url     : 'http://127.0.0.1:8545',
+    method  : 'post',
+    headers : { 'Content-Type': 'application/json' },
+    body    : { jsonrpc: '2.0', id: 0, params: [] }
+  };
+  let options = Object.assign({}, standardOptions);
+
+  //add ether-specific props to body
+  Object.assign(options.body, o);
+
+  //format body to str
+  options.body = JSON.stringify(options.body);
+
+  return options;
+}
+
+module.exports = utils;
